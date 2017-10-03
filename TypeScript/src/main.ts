3fd5dc509d0dcf4e488617daf351e3137ca0b490
ManfredSteyer <manfred.steyer@gmx.net>
2017-10-03 10:31:44 +0200

    jquery

--- TypeScript/src/main.ts
+++ TypeScript/src/main.ts
@@ -7,23 +7,62 @@ import { Flight } from './flights/flight';
 import { FlightManager } from './flights/flight-manager';
 import { ExtFlightManager } from './flights/ext-flight-manager';
 
+// Nicht vergessen!
+import * as $ from 'jquery';
+
+
 let f1 = new Flight(17);
 f1.to = 'Graz';
 f1.from = 'Hamburg';
 f1.date = '2017-10-03T17:00+01:00';
 
 let f2 = new Flight(18);
-f2.to = 'Hamburg';
-f2.from = 'Graz';
+f2.to = 'Graz';
+f2.from = 'Hamburg';
 f2.date = '2017-10-05T17:00+01:00';
 
-let fm = new ExtFlightManager([], 1);
-// fm.add(f1);
+
+let f3 = new Flight(19);
+f3.to = 'Hamburg';
+f3.from = 'Graz';
+f3.date = '2017-10-05T17:00+01:00';
+
+let fm = new ExtFlightManager([], 100);
+fm.add(f1);
 fm.add(f2);
+fm.add(f3);
 
 let result = fm.findById(18);
 console.debug('found flight', result);
+console.debug('found flightId', result.id);
+
 console.debug('flights count', fm.count);
+
+
+// GUI
+
+$('#btnSearch').click(() => {
+
+  let from: string = $('#from').val() as string;
+  let to: string = $('#to').val() as string;
+
+  let flights = fm.findByRoute(from, to);
+
+  let html = '<table>';
+  for(let f of flights) {
+    html += `
+      <tr>
+        <td>${f.id}</td>
+        <td>${f.from}</td>
+        <td>${f.to}</td>
+        <td>${f.date}</td>
+      </tr>
+    `;
+  }
+  html += "</table>";
+
+  $('#result').html(html);
+});
 
 
 
